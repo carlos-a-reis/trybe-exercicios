a7994055b2f701351fe460fd8cbde3a24426ae1e
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-02-02 16:24:53 -0300

    Adiciona primeiro exercicio de JavaScript

---
+++ fundamentos/JavaScript/dia-1-JavaScript/scripts.js
@@ -0,0 +1,16 @@
+//const myName = "Carlos";
+//const birthCity = "Itarana";
+//let birthYear = 2001;
+//birthYear = 2030;
+
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = 2 * base + 2 * height;
+
+console.log(area);
+console.log(perimeter);
