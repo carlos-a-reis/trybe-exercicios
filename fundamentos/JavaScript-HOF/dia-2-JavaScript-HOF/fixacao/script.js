ca4c834e2511d77b34813044293e2a410b679f2f
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-10 14:12:45 -0300

    Exercicios de fixacao do dia

---
+++ fundamentos/JavaScript-HOF/dia-2-JavaScript-HOF/fixacao/script.js
@@ -0,0 +1,50 @@
+// parte 1
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  };
+  
+emailListInData.forEach(showEmailList);
+
+// parte 2
+// 1
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+    const result = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+    return result;
+};
+
+console.log(findDivisibleBy3And5());
+
+// 2
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  const result = names.find((nome) => nome.length === 5);
+  return result;
+}
+
+console.log(findNameWithFiveLetters());
+
+// 3
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    const result = musicas.find((musica) => musica.id === id);
+    return result.title;
+  }
+  
+  console.log(findMusic('31031685'))
+
+  // 3
