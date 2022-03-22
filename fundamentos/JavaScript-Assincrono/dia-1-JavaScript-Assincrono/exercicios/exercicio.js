a30602a7f5c592ddf31a71ba66b3cd6e36623084
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-22 08:57:28 -0300

    Segundo exercicio dia 9.1

--- fundamentos/JavaScript-Assincrono/dia-1-JavaScript-Assincrono/exercicios/exercicio.js
+++ fundamentos/JavaScript-Assincrono/dia-1-JavaScript-Assincrono/exercicios/exercicio.js
@@ -31,3 +31,37 @@ console.log(planetDistanceFromSun(venus)); // B
 console.log(planetDistanceFromSun(jupiter)); // C
 
 // ordem: A, B e C
+
+// exercicio 2
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// ordem: A, C e B
