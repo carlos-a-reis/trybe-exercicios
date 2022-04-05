0b5900117bdb4a1478581ca6accd423af951de26
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-04-05 17:52:07 -0300

    Versao final da pokedex

--- Front-end/Introducao-a-React/dia-2-Introducao-a-React/my-pokedex/src/Pokemon.js
+++ Front-end/Introducao-a-React/dia-2-Introducao-a-React/my-pokedex/src/Pokemon.js
@@ -4,9 +4,11 @@ class Pokemon extends React.Component {
     render(){
         return (
             <div className='pokemon-info'>
-                <span className='pokemon'>{this.props.pokemon.name}</span>
-                <span className='pokemon'>{this.props.pokemon.type}</span>
-                <span className='pokemon'>Average Weight: {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</span>
+                <div className='pokemon'>
+                    <span>{this.props.pokemon.name}</span>
+                    <span>{this.props.pokemon.type}</span>
+                    <span>Average Weight: {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</span>
+                </div>
                 <a target='blank' href={this.props.pokemon.moreInfo}>
                     <img className='img' src={this.props.pokemon.image} alt={this.props.pokemon.name}/>
                 </a>
